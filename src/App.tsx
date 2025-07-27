8a7ba68ed22c751d4a0e83d4050fb9ea06621fef
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-27 13:24:27 +0200

    added useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import './App.css'
 import {Task, TodolistItem} from "./TodolistItem.tsx";
+import {useState} from "react";
 
 
 export const App = () => {
 
     // TODO BLL
     const todolistTitle = 'What to learn'
-    let tasks: Task[] = [
+
+    const [tasks, setTasks] = useState<Task[]>([
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'JS', isDone: true},
         {id: 3, title: 'ReactJS', isDone: false},
-    ]
+    ])
 
     const deleteTask = (taskId: Task['id']) => {
         tasks = tasks.filter(task => task.id !== taskId)
         console.log(tasks)
     }
+
 
 
     //   TODO UI
