200864ac784ccb455e7c871cf53fbcfef34dc38c
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-07-27 12:33:24 +0200

    init lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,19 @@ import {Task, TodolistItem} from "./TodolistItem.tsx";
 
 export const App = () =>  {
 
-    // TODO BL
-    const todolistTitle1 = 'What to learn'
-    const todolistTitle2 = 'What to buy'
-    const tasks1: Task[] = [
+    // TODO BLL
+    const todolistTitle = 'What to learn'
+    const tasks: Task[] = [
         { id: 1, title: 'HTML&CSS', isDone: true },
         { id: 2, title: 'JS', isDone: true },
         { id: 3, title: 'ReactJS', isDone: false },
     ]
-    const tasks2: Task[] = [
-        // { id: 1, title: 'Hello world', isDone: true },
-        // { id: 2, title: 'I am Happy', isDone: false },
-        // { id: 3, title: 'Yo', isDone: false },
-    ]
 
+
+  //   TODO UI
   return (
       <div className="app">
-        <TodolistItem title={todolistTitle1} tasks={tasks1}/>
-        <TodolistItem title={todolistTitle2} tasks={tasks2}/>
+        <TodolistItem title={todolistTitle} tasks={tasks}/>
       </div>
   )
 }
