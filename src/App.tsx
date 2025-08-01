681a080562991a25fdcc7628baa6e80638631b5e
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-08-01 14:30:05 +0200

    added state for tasks

--- src/App.tsx
+++ src/App.tsx
@@ -27,17 +27,20 @@ export const App = () => {
     ])
 
     const [tasks, setTasks] = useState<TasksState>({
-
+        [todolistId_1]: [
+            {id: v1(), title: 'HTML&CSS', isDone: true},
+            {id: v1(), title: 'JS', isDone: true},
+            {id: v1(), title: 'ReactJS', isDone: false},
+        ],
+        [todolistId_2]: [
+            {id: v1(), title: 'Milk', isDone: true},
+            {id: v1(), title: 'Water', isDone: true},
+            {id: v1(), title: 'Cheese', isDone: false},
+        ],
     })
 
 
     // TODO BLL
-
-    const [tasks, setTasks] = useState<Task[]>([
-        {id: v1(), title: 'HTML&CSS', isDone: true},
-        {id: v1(), title: 'JS', isDone: true},
-        {id: v1(), title: 'ReactJS', isDone: false},
-    ])
 
     // TODO - Create
 
