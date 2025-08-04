a65ec7487a3663d30b519849f6fc7daa4fffc70b
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-08-04 11:34:00 +0200

    added setItemTitle

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,10 +1,22 @@
+import {ChangeEvent, useState} from "react";
 
 type EditableSpanTypes = {
     title: string
 };
 
 export const EditableSpan = ({title}: EditableSpanTypes) => {
+    const [itemTitle, setItemTitle] = useState('');
+    const [isEditMode, setIsEditMode] = useState(false);
+    const onChangeItemTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setItemTitle(e.currentTarget.value)
+    }
+
     return (
-        <span>{title}</span>
+        isEditMode
+            ? <input
+                value={itemTitle}
+                onChange={onChangeItemTitleHandler}
+            />
+            : <span>{title}</span>
     );
 };
