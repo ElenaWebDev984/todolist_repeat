a819081d8049c583b3aa41b4f984ca1837e58b47
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-08-14 20:07:41 +0200

    added ActionType

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -2,8 +2,9 @@ import {Todolist} from "../App.tsx";
 
 export type DeleteTodolistActionType = ReturnType<typeof deleteTodolistAC>
 export type CreateTodolistActionType = ReturnType<typeof createTodolistAC>
+type ActionType = DeleteTodolistActionType | CreateTodolistActionType
 
-export const todolistsReducer = (todolists: Todolist[], action: DeleteTodolistActionType | CreateTodolistActionType):Todolist[] => {
+export const todolistsReducer = (todolists: Todolist[], action: ActionType):Todolist[] => {
     switch (action.type) {
         case 'delete_todolist':
             return todolists.filter(todolist => todolist.id !== action.payload.id)
