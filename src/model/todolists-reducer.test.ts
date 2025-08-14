00c7d545d8a4021dad7d36e726fb6972d55afbdd
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-08-14 18:45:04 +0200

    added as const for types

--- src/model/todolists-reducer.test.ts
+++ src/model/todolists-reducer.test.ts
@@ -1,30 +1,40 @@
 import {v1} from 'uuid'
 import { expect, test } from 'vitest'
 import type {Todolist} from '../App'
-import {todolistsReducer} from './todolists-reducer'
+import {todolistsReducer} from "./todolists-reducer.ts";
+// import {DeleteTodolistActionType} from './todolists-reducer'
 
 test('correct todolist should be deleted', () => {
     const todolistId1 = v1()
     const todolistId2 = v1()
 
-    // 1. Стартовый state
+    // 1. TODO Стартовый state
     const startState: Todolist[] = [
         {id: todolistId1, title: 'What to learn', filter: 'all'},
         {id: todolistId2, title: 'What to buy', filter: 'all'},
     ]
 
-    // 2. Действие
+    // 2. TODO Действие
+    // TODO variant 1
     const action = {
-        type: 'delete_todolist',
+        type: 'delete_todolist' as const,
         payload: {
             id: todolistId1,
         },
     }
+    // TODO variant 2
+    // const action: DeleteTodolistActionType = {
+    //     type: 'delete_todolist',
+    //     payload: {
+    //         id: todolistId1,
+    //     },
+    // }
+
     const endState = todolistsReducer(startState, action)
 
-    // 3. Проверка, что действие измененило state соответствующим образом
-    // в массиве останется один тудулист
+    // 3. TODO Проверка, что действие измененило state соответствующим образом
+    // TODO в массиве останется один тудулист
     expect(endState.length).toBe(1)
-    // удалится нужный тудулист, не любой
+    // TODO удалится нужный тудулист, не любой
     expect(endState[0].id).toBe(todolistId2)
 })
